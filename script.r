ccc18e3ed14ad73842830bc8c5a2dadfb2ca9f43
Nícolas Duarte <nicolasmd9@hotmail.com>
2021-08-19 09:30:31 -0300

    Update script.r

--- script.r
+++ script.r
@@ -4,15 +4,17 @@ library(ggplot2)
 names <- c("horario", "temp", "vento", "umid", "sensa")
 con <- url("http://ic.unicamp.br/~zanoni/cepagri/cepagri.csv")
 cepagri <- read.csv(con, header = FALSE, sep = ";", col.names = names)
-                    
-horario <- as.data.frame(cepagri$horario)
-temp <- as.data.frame(cepagri$temp)
-vento <- as.data.frame(cepagri$vento)
-umid <- as.data.frame(cepagri$umid)
-sensa <- as.data.frame(cepagri$sensa)
+
+horario <- cepagri$horario
+temp <- cepagri$temp
+vento <- cepagri$vento
+umid <- cepagri$umid
+sensa <- cepagri$sensa
+
+df <- data.frame(100:100, horario, temp)
 
 pdf("grafico.pdf") # Indica o arquivo que o gráfico será salvo
-p <- ggplot(horario, aes(x = horario,y = temp)) # Gera o gráfico
+p <- ggplot(df, aes(x = horario,y = temp)) # Gera o gráfico
 p <- p + geom_point() # Adiciona camada: Gráfico de Pontos
 p <- p + geom_line() # Adiciona camada: Gráfico de Linha
 print(p) # Salva o gráfico no arquivo indicado
