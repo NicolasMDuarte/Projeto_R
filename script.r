d8aec246933aa5a06919b34988553a2f20804510
Eduardo Migueis <57845961+edumigueis@users.noreply.github.com>
2021-08-26 15:57:52 -0300

    Plot Saving and Color Adjustments

--- script.r
+++ script.r
@@ -1,5 +1,5 @@
-install.packages("ggplot2") #instalando pacote
 library(ggplot2) #usando biblioteca do ggplot2
+library(cowplot)
 
 names <- c("horario", "temp", "vento", "umid", "sensa") #nomes dos campos
 con <- url("http://ic.unicamp.br/~zanoni/cepagri/cepagri.csv") #conexao com o link dos dados
@@ -28,60 +28,47 @@ data <- data[data$sensa <= 40,]
 
 data <- na.omit(data) #remocao de dados incompletos ou ausentes
 
-horSensa <- ggplot(data, aes(x = horario, y = sensa, color=sensa)) #desenho dos graficos
-horTemp <- ggplot(data, aes(x = horario, y = temp, color=temp)) #desenho dos graficos
-horVent <- ggplot(data, aes(x = horario, y = vento, color=vento)) #desenho dos graficos
-horUmid <- ggplot(data, aes(x = horario, y = umid, color=umid)) #desenho dos graficos
-tempSensa <- ggplot(data, aes(x = temp, y = sensa, color=sensa)) #desenho dos graficos
-sensaVent <- ggplot(data, aes(x = sensa, y = vento, color=vento)) #desenho dos graficos
-
-#desenho das analises
-horSensa +
+horSensa <- ggplot(data, aes(x = horario, y = sensa, color=sensa)) +
   geom_point(alpha = 0.1) +
-  labs(x = "Horário", y = "Sensação Térmica(°C)", color = "Sensação Térmica(°C)") +
+  labs(x = "Horário", y = "Sensação Térm.", color = "Sensação Térm.") +
   coord_cartesian(ylim=c(-10,40)) +
-  scale_color_gradient(low="blue", high="red")
-horTemp +
+  scale_color_gradient(low="turquoise3", high="violetred2") #desenho dos graficos
+horTemp <- ggplot(data, aes(x = horario, y = temp, color=temp)) +
   geom_point(alpha = 0.1) +
   labs(x = "Horário", y = "Temperatura (°C)", color = "Temperatura (°C)") +
   coord_cartesian(ylim=c(0,40)) +
-  scale_color_gradient(low="blue", high="red")
-horVent +
+  scale_color_gradient(low="coral", high="red") #desenho dos graficos
+horVent <- ggplot(data, aes(x = horario, y = vento, color=vento)) +
   geom_point(alpha = 0.1) +
   labs(x = "Horário", y = "Vento", color = "Vento") +
-  coord_cartesian(ylim=c(0,150)) +
-  scale_color_gradient(low="blue", high="red")
-horUmid +
+  coord_cartesian(ylim=c(0,125)) +
+  scale_color_gradient(low="seagreen1", high="seagreen") #desenho dos graficos
+horUmid <- ggplot(data, aes(x = horario, y = umid, color=umid)) +
   geom_point(alpha = 0.1) +
   labs(x = "Horário", y = "Umidade", color = "Umidade") +
   coord_cartesian(ylim=c(-20,100)) +
-  scale_color_gradient(low="blue", high="red")
-tempSensa +
+  scale_color_gradient(low="deepskyblue", high="deepskyblue4")#desenho dos graficos
+tempSensa <- ggplot(data, aes(x = temp, y = sensa, color=sensa)) +
   geom_point(alpha = 0.1) +
-  labs(x = "Temperatura (°C)", y = "Sensação Térmica(°C)", color = "Sensação Térmica(°C)") +
+  labs(x = "Temperatura (°C)", y = "Sensação Térm.", color = "Sensação Térm.") +
   coord_cartesian(ylim=c(-10,40)) +
-  scale_color_gradient(low="blue", high="red")
-sensaVent +
+  scale_color_gradient(low="turquoise3", high="violetred2") #desenho dos graficos
+sensaVent <- ggplot(data, aes(x = sensa, y = vento, color=vento)) +
   geom_point(alpha = 0.1) +
-  labs(x = "Sensação Térmica(°C)", y = "Vento", color = "Vento") +
-  coord_cartesian(ylim=c(0,150)) +
-  scale_color_gradient(low="blue", high="red")
+  labs(x = "Sensação Térm.", y = "Vento", color = "Vento") +
+  coord_cartesian(ylim=c(0,125)) +
+  scale_color_gradient(low="seagreen1", high="seagreen") #desenho dos graficos
 
+plot_grid(horSensa, horTemp, horVent, horUmid, tempSensa, sensaVent, labels = "AUTO")
+sv <- plot_grid(horSensa, horTemp, horVent, horUmid, tempSensa, sensaVent, labels = "AUTO")
+save_plot("plot.png", sv, ncol = 2)
 #Salvando os graficos - NAO ESTA SALVANDO OS DADOS - TENTAR FAZER EM PNG OU SEI LA
-pdf("grafico_horSensa.pdf") 
-print(horSensa)
-pdf("grafico_horTemp.pdf") 
-print(horTemp)
-pdf("grafico_horVent.pdf") 
-print(horVent)
-pdf("grafico_horUmid.pdf") 
-print(horUmid)
-pdf("grafico_tempSensa.pdf")
-print(tempSensa)
-pdf("grafico_sensaVent.pdf") 
-print(sensaVent)
+#ggsave("grafico_horSensa.png", plot = horSensa)
+#ggsave("grafico_horTemp.png", plot = horTemp)
+#ggsave("grafico_horVent.png", plot = horVent)
+#ggsave("grafico_horUmid.png", plot = horUmid)
+#ggsave("grafico_tempSensa.png", plot = tempSensa)
+#ggsave("grafico_sensaVent.png", plot = sensaVent)
 
-dev.off () # Fecha o arquivo
-
-remove.packages('ggplot2') #remove os pacotes
+#dev.off () # Fecha o arquivo
 rm(list=ls()) #remove as variaveis
