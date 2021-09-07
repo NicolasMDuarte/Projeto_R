a5e3988aa1d37ae2619c53e0a70bb643641cf4cd
Nícolas Duarte <nicolasmd9@hotmail.com>
2021-09-06 21:40:12 -0300

    Update 19167_19192_19197_Script.r

--- 19167_19192_19197_Script.r
+++ 19167_19192_19197_Script.r
@@ -1,4 +1,5 @@
-library(ggplot2) #usando biblioteca do ggplot2
+#bibliotecas
+library(ggplot2)
 library(cowplot)
 library(dplyr)
 library(magrittr)
@@ -64,29 +65,28 @@ sensaVent <- ggplot(data, aes(x = sensa, y = vento, color=vento)) +
   coord_cartesian(ylim=c(0,125)) +
   scale_color_gradient(low="seagreen1", high="seagreen") 
 
-mMes <- data %>%
+#outros tipos de graficos
+mMes <- data %>% #media nos meses
   mutate(dia = floor_date(as.Date(horario, unit="months", format="%d/%m/%Y"))) %>%
   group_by(dia) %>%
   summarize(Temperatura = mean(as.numeric(temp)), Sensacao = mean(sensa), mes=month(dia))
-tempAno <- ggplot(mMes, aes(x= Temperatura, y = as.factor(mes), fill = stat(x))) + 
+tempAno <- ggplot(mMes, aes(x= Temperatura, y = as.factor(mes), fill = stat(x))) + #grafico de temperatura no ano
   geom_density_ridges_gradient(scale = 2, rel_min_height = 0.01) +
   labs(x = "Temperatura (�C)", y = "M�dia dos meses") + 
   scale_fill_viridis_c(name = "Temp. [C]", option = "C") + 
   theme_ridges(grid = TRUE)
-
-mAno_temp <- data %>%
+mAno_temp <- data %>% #media nos anos
   mutate(Ano = floor_date(unit = "year", x = as.Date(horario, format="%d/%m/%Y"))) %>%
   group_by(Ano) %>%
   summarize(Temperatura = mean(as.numeric(temp)))
-tempBarAno <- ggplot(mAno_temp, aes(x = Ano, y = Temperatura)) + geom_col(colour="coral", fill="coral") + coord_cartesian(ylim= c(21,23))
-
-mUmid_temp <- data %>%
+tempBarAno <- ggplot(mAno_temp, aes(x = Ano, y = Temperatura)) + geom_col(colour="coral", fill="coral") + coord_cartesian(ylim= c(21,23)) #grafico de barras
+mUmid_temp <- data %>% #media de umidade
   mutate(Ano = floor_date(unit = "year", x = as.Date(horario, format="%d/%m/%Y"))) %>%
   group_by(Ano) %>%
   summarize(Umidade = mean(as.numeric(umid)))
-umidAno <- ggplot(mUmid_temp, aes(x = Ano, y = Umidade)) + geom_line(color="deepSkyBlue4") + coord_cartesian(ylim= c(0,100))
+umidAno <- ggplot(mUmid_temp, aes(x = Ano, y = Umidade)) + geom_line(color="deepSkyBlue4") + coord_cartesian(ylim= c(0,100)) #grafico de umidade no ano
 
-
+#salva os dados
 save_plot("plot.png", plot_grid(horSensa, horTemp, horVent, horUmid, tempSensa, sensaVent, tempAno, tempBarAno, umidAno, labels = "AUTO"), ncol = 2, base_height = 10)
 
 rm(list=ls()) #remove as variaveis
